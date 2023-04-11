01c9eec02db99ff24e63e6c494e1e69c86f1ab70
Po7di <henszkew@gmail.com>
2023-04-11 12:19:19 +0200

    Initial Commit

---
+++ recipe-list-app/recipe_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_list_appApp.swift
+//  recipe-list-app
+//
+//  Created by Waldemar Henszke on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct recipe_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
