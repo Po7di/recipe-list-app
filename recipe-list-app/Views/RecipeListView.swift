cb3a8ea275451394cbd6a6887b66b17d6bbba2f8
Po7di <henszkew@gmail.com>
2023-04-25 16:06:01 +0200

    Adding he NavigationView, NavigationLink and recipe detail view

--- recipe-list-app/Views/RecipeListView.swift
+++ recipe-list-app/Views/RecipeListView.swift
@@ -14,17 +14,30 @@ struct RecipeListView: View {
     
     var body: some View {
         
-        List(model.recipes) { r in
-            
-            HStack(spacing: 20.0) {
-                Image(r.image)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 50, height: 50, alignment: .center)
-                    .clipped()
-                    .cornerRadius(5)
-                Text(r.name)
+        NavigationView {
+            List(model.recipes) { r in
+                
+                NavigationLink(
+                    destination: RecipeDetailView(recipe:r),
+                    label: {
+                        
+                        // MARK: Row item
+                        HStack(spacing: 20.0) {
+                            Image(r.image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 50, height: 50, alignment: .center)
+                                .clipped()
+                                .cornerRadius(5)
+                            Text(r.name)
+                        }
+                        
+                    })
+                
+                
+                
             }
+            .navigationBarTitle("All Recipes")
         }
     }
 }
